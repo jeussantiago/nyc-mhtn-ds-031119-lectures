fe19c765b7ac4e55f94c9fcda5add0e7746aed7e
Jeus Santiago <jeus1santiago@gmail.com>
2019-03-11 17:38:13 -0400

    updated instructions to give examples.

--- functions.py
+++ functions.py
@@ -1,17 +1,24 @@
 # Make a function to determine if a number is odd or even
 
-def odd_even():
-    pass
+def odd_even(num):
+    if num % 2 == 0:
+        return "even"
+    else:
+        return "odd"
 
 # Make a function that takes in a list of numbers and returns the numbers that are even
 
 def even_list(numbers):
-    pass
+    return list(filter(lambda num: num % 2 == 0, numbers))
 
 # Given a list return the unique names in the list
 
 def unique_names(list_of_names):
-    pass
+    unique_names = []
+    for name in list_of_names:
+        if name not in unique_names:
+            unique_names.append(name)
+    return unique_names
 
 # Make a function that determines if a word is a palindrome
 
@@ -19,7 +26,12 @@ def palindrome_detector(string):
     """
     Input: string
     returns : True/False"""
-    pass
+    reverse_str = string[::-1]
+    if string == reverse_str:
+        return True
+    else:
+        return False
+    
 
 print(odd_even(4))
 print(odd_even(139))
